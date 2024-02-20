d478a8e60c733e45977a69b468f7ae281605d5f6
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-02-20 23:25:43 +0000

    'implemented' discord support

--- src/main/java/org/rolypolyvole/villagerdeaths/VillagerDeathMessagesPlugin.java
+++ src/main/java/org/rolypolyvole/villagerdeaths/VillagerDeathMessagesPlugin.java
@@ -1,10 +1,17 @@
 package org.rolypolyvole.villagerdeaths;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.rolypolyvole.villagerdeaths.event.VillagerDeathListener;
 
 public final class VillagerDeathMessagesPlugin extends JavaPlugin {
+
+    private final Plugin discordSRV = Bukkit.getPluginManager().getPlugin("DiscordSRV");
+
+    public Plugin getDiscordSRV() {
+        return discordSRV;
+    }
 
     @Override
     public void onEnable() {
